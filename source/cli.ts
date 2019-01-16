46e93c7f182ab7cf0f548ccf37a7312dfdaad24c
Ari Chivukula <ari@chivuku.la>
2019-01-16 10:46:37 -0800

    Create cli.ts

---
+++ source/cli.ts
@@ -0,0 +1,26 @@
+import * as yargs from "yargs";
+
+import {
+  sitation,
+} from "./index";
+
+if (require.main === module) {
+  yargs
+    .usage(
+      "$0",
+      true,
+      (y: yargs.Argv<any>): yargs.Argv<any> => y
+        .option(
+          "casebody",
+          {
+            demandOption: true,
+            describe: "Case Body Text",
+          },
+        ),
+      (argv: yargs.Arguments<any>): void => {
+        console.log(sitation(argv.casebody));
+      },
+    )
+    .help()
+    .argv;
+}
