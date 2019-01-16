34be58daeb4f43aa0a11fe817540f6d5bfb89526
Ari Chivukula <ari@chivuku.la>
2019-01-16 07:08:19 -0800

    Update data.ts

--- source/data.ts
+++ source/data.ts
@@ -18,11 +18,10 @@ export type Reporters = {
 let _REPORTERS: Reporters | null = null;
 
 export function reporters(): Reporters {
-  let r = _REPORTERS;
-  if (r === null) {
-    r = JSON.parse(readFileSync("reporters.json", "ascii"));
+  if (_REPORTERS === null) {
+    _REPORTERS = JSON.parse(readFileSync("reporters.json", "ascii"));
   }
-  return r;
+  return _REPORTERS as Reporters;
 }
 
 export type Editions = string[];
@@ -30,11 +29,10 @@ export type Editions = string[];
 let _EDITIONS: Editions | null = null;
 
 export function editions(): Editions {
-  let e = _EDITIONS;
-  if (e === null) {
-    e = Object.values(reporters()).map((r: Reporter) => Object.keys(r.editions)).flat(1);
+  if (_EDITIONS === null) {
+    _EDITIONS = Object.values(reporters()).map((r: Reporter) => Object.keys(r.editions)).flat(1);
   }
-  return e;
+  return _EDITIONS as Editions;
 }
 
 export type Variations = { [k: string]: string };
