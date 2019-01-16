63c21b4813ad12754d326b409fb3217103cde481
Ari Chivukula <ari@chivuku.la>
2019-01-16 06:47:10 -0800

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,10 +1,3 @@
 import "@babel/polyfill";
 
-import {
-  readFileSync,
-} from "fs";
-
-export function noop() {
-  const data = readFileSync("reporters.json", "ascii");
-  JSON.parse(data);
-}
+export * from "./data";
