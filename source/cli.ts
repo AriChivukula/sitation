c868c92c6d8960cab5799f545bf2b3a550aa125b
Ari Chivukula <ari@chivuku.la>
2019-01-16 10:49:27 -0800

    Update cli.ts

--- source/cli.ts
+++ source/cli.ts
@@ -6,23 +6,21 @@ import {
   sitation,
 } from "./index";
 
-if (require.main === module) {
-  yargs
-    .usage(
-      "$0",
-      true,
-      (y: yargs.Argv<any>): yargs.Argv<any> => y
-        .option(
-          "casebody",
-          {
-            demandOption: true,
-            describe: "Case Body Text",
-          },
-        ),
-      (argv: yargs.Arguments<any>): void => {
-        console.log(sitation(argv.casebody));
-      },
-    )
-    .help()
-    .argv;
-}
+yargs
+  .usage(
+    "$0",
+    true,
+    (y: yargs.Argv<any>): yargs.Argv<any> => y
+      .option(
+        "casebody",
+        {
+          demandOption: true,
+          describe: "Case Body Text",
+        },
+      ),
+    (argv: yargs.Arguments<any>): void => {
+      console.log(sitation(argv.casebody));
+    },
+  )
+  .help()
+  .argv;
