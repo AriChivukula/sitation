a00ebab958fff5fd8314a95bbfe6e685b7d8394e
Ari Chivukula <ari@chivuku.la>
2019-01-16 09:29:35 -0800

    Create token.ts

---
+++ test/token.ts
@@ -0,0 +1,29 @@
+import "mocha";
+
+import * as chai from "chai";
+
+import {
+  tokenize,
+  Token,
+} from "../source/index";
+
+describe("tokenize()", function() {
+  [
+    {
+      casebody: "",
+      expected: [],
+    },
+    {
+      casebody: "379 U. S. 241",
+      expected: [("379", Token.NUMBER), ("U.S.", Token.REPORTER), ("241", Token.NUMBER)],
+    },
+    {
+      casebody: "In Evans v. Laurel Links, Inc., id.",
+      expected: [("In", Token.NOOP), ("Evans", Token.NOOP), ("v", Token.NOOP), ("Laurel", Token.NOOP), ("Links", Token.NOOP), ("Inc", Token.NOOP), ("Id", Token.ID)],
+    },
+  ].forEach((test) => {
+    it(test.casebody, () => {
+      chai.assert(tokenize(test.casebody)).equals(test.expected);
+    });
+  });
+});
