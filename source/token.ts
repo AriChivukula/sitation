fe334450d5bb85d94648e5e56b08214c55f92e02
Ari Chivukula <ari@chivuku.la>
2019-01-16 09:31:53 -0800

    Update token.ts

--- source/token.ts
+++ source/token.ts
@@ -20,19 +20,19 @@ export function tokenize(casebody: string): Tokenized[] {
   const tokens: Tokenized[] = [];
   for (let reporter_split_token of casebody.split(reporterRegExp())) {
     if (editions().hasOwnProperty(reporter_split_token)) {
-      tokens.push((reporter_split_token, Token.REPORTER));
+      tokens.push([reporter_split_token, Token.REPORTER]);
     } else if (variations().hasOwnProperty(reporter_split_token)) {
-      tokens.push((variations()[reporter_split_token], Token.REPORTER));
+      tokens.push([variations()[reporter_split_token], Token.REPORTER]);
     } else {
       for (let spacing_split_token of reporter_split_token.split(spacingRegExp())) {
         if (spacing_split_token == "") {
           continue;
         } else if (!isNaN(spacing_split_token)) {
-          tokens.push((spacing_split_token, Token.NUMBER))
+          tokens.push([spacing_split_token, Token.NUMBER])
         } else if (spacing_split_token.toLowerCase() == "id") {
-          tokens.push(("Id", Token.ID));
+          tokens.push(["Id", Token.ID]);
         } else {
-          tokens.push((spacing_split_token, Token.NOOP))
+          tokens.push([spacing_split_token, Token.NOOP])
         }
       }
     }
