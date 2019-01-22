3c639083068b59fad34fb5196c38054ca8bf2709
Ari Chivukula <ari@chivuku.la>
2019-01-22 10:52:04 -0800

    Update data.ts

--- source/data.ts
+++ source/data.ts
@@ -1,6 +1,9 @@
 import {
   readFileSync,
 } from "fs";
+import {
+  MemoizeAll,
+} from "lodash-decorators";
 
 export type Reporter = {
   editions: {
@@ -15,47 +18,39 @@ export type Reporters = {
   [k: string]: Reporter[],
 };
 
-let _REPORTERS: Reporters | null = null;
-
-export function reporters(): Reporters {
-  if (_REPORTERS === null) {
-    _REPORTERS = JSON.parse(readFileSync("reporters.json", "ascii"));
-  }
-  return _REPORTERS as Reporters;
-}
-
 export type Editions = { [k: string]: string };
-
-let _EDITIONS: Editions | null = null;
-
-export function editions(): Editions {
-  if (_EDITIONS === null) {
-    _EDITIONS = {};
-    for (let reporterName in reporters()) {
-      for (let reporter of reporters()[reporterName]) {
-        for (let edition in reporter.editions) {
-          _EDITIONS[edition.toLowerCase()] = edition;
-        }
-      }
-    }
-  }
-  return _EDITIONS as Editions;
-}
 
 export type Variations = { [k: string]: string };
 
-let _VARIATIONS: Variations | null = null;
+export abstract class ReportersDB {
 
-export function variations(): Variations {
-  if (_VARIATIONS === null) {
-    _VARIATIONS = {};
+  @MemoizeAll
+  public static reporters(): Reporters {
+    return JSON.parse(readFileSync("reporters.json", "ascii"));
+  }
+
+  @MemoizeAll
+  public static editions(): Editions {
+    let result = {};
     for (let reporterName in reporters()) {
       for (let reporter of reporters()[reporterName]) {
-        for (let variation in reporter.variations) {
-          _VARIATIONS[variation.toLowerCase()] = reporter.variations[variation];
+        for (let edition in reporter.editions) {
+          result[edition.toLowerCase()] = edition;
         }
       }
     }
+    return result;
   }
-  return _VARIATIONS as Variations;
-}
+
+  @MemoizeAll
+  public static variations(): Variations {
+    let result = {};
+    for (let reporterName in reporters()) {
+      for (let reporter of reporters()[reporterName]) {
+        for (let variation in reporter.variations) {
+          result[variation.toLowerCase()] = reporter.variations[variation];
+        }
+      }
+    }
+    return result;
+  }
