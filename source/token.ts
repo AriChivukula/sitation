f10dfd94d5233a2f4c90e944128bead4f1367ba6
Ari Chivukula <ari@chivuku.la>
2019-01-22 10:26:11 -0800

    Update token.ts

--- source/token.ts
+++ source/token.ts
@@ -18,14 +18,14 @@ export type Tokenized = [string, string, Token];
 
 export function tokenize(casebody: string): Tokenized[] {
   const tokens: Tokenized[] = [];
-  for (let reporter_split_token of casebody.split(reporterRegExp())) {
+  for (let reporter_split_token of casebody.split(reporterRE())) {
     const lower_split_token = reporter_split_token.toLowerCase();
     if (editions().hasOwnProperty(lower_split_token)) {
       tokens.push([editions()[lower_split_token], reporter_split_token, Token.REPORTER]);
     } else if (variations().hasOwnProperty(lower_split_token)) {
       tokens.push([variations()[lower_split_token], reporter_split_token, Token.REPORTER]);
     } else {
-      for (let spacing_split_token of reporter_split_token.split(spacingRegExp())) {
+      for (let spacing_split_token of reporter_split_token.split(spacingRE())) {
         if (spacing_split_token == "") {
           continue;
         } else if (!isNaN(Number(spacing_split_token))) {
