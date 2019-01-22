ee0b24045f045968d4b8fd65a7d897a0d6e5355a
Ari Chivukula <ari@chivuku.la>
2019-01-22 14:10:01 -0800

    Refactor

--- source/token.ts
+++ source/token.ts
@@ -12,26 +12,33 @@ export enum Token {
   REPORTER, // Can be found in `REPORTER_SET`
 }
 
-export type Tokenized = [string, string, Token];
+export class Segment {
+  constructor(
+    public corrected: string,
+    public original: string,
+    public token: Token,
+  ) {
+  }
+}
 
-export function tokenize(casebody: string): Tokenized[] {
-  const tokens: Tokenized[] = [];
+export function tokenize(casebody: string): Segment[] {
+  const tokens: Segment[] = [];
   for (let reporter_split_token of casebody.split(Expressions.reporter())) {
     const lower_split_token = reporter_split_token.toLowerCase();
     if (ReportersDB.editions().hasOwnProperty(lower_split_token)) {
-      tokens.push([ReportersDB.editions()[lower_split_token], reporter_split_token, Token.REPORTER]);
+      tokens.push(new Segment(ReportersDB.editions()[lower_split_token], reporter_split_token, Token.REPORTER));
     } else if (ReportersDB.variations().hasOwnProperty(lower_split_token)) {
-      tokens.push([ReportersDB.variations()[lower_split_token], reporter_split_token, Token.REPORTER]);
+      tokens.push(new Segment(ReportersDB.variations()[lower_split_token], reporter_split_token, Token.REPORTER));
     } else {
       for (let spacing_split_token of reporter_split_token.split(Expressions.spacing())) {
         if (spacing_split_token == "") {
           continue;
         } else if (!isNaN(Number(spacing_split_token))) {
-          tokens.push([spacing_split_token, spacing_split_token, Token.NUMBER])
+          tokens.push(new Segment(spacing_split_token, spacing_split_token, Token.NUMBER))
         } else if (spacing_split_token.toLowerCase() == "id") {
-          tokens.push(["Id", spacing_split_token, Token.ID]);
+          tokens.push(new Segment("Id", spacing_split_token, Token.ID));
         } else {
-          tokens.push([spacing_split_token, spacing_split_token, Token.NOOP])
+          tokens.push(new Segment(spacing_split_token, spacing_split_token, Token.NOOP))
         }
       }
     }
