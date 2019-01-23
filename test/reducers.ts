18e5b2f84e8245aaa089816e68a326049c980620
Ari Chivukula <ari@chivuku.la>
2019-01-23 10:39:16 -0800

    Update reducers.ts

--- test/reducers.ts
+++ test/reducers.ts
@@ -7,12 +7,13 @@ import {
   MapperType,
 } from "../source/mapper";
 import {
-  idCite,
-  fullCite,
+  idConsume,
+  fullConsume,
+  rootReducer,
 } from "../source/reducers";
 
 describe(
-  "idCite()",
+  "idConsume()",
   () => {
     [
       {
@@ -27,7 +28,7 @@ describe(
       it(
         test.results.join(","),
         () => {
-          chai.expect(idCite(test.results).toString()).to.equal(test.expected);
+          chai.expect(idConsume(test.results).join(",")).to.equal(test.expected);
         },
       );
     });
@@ -35,7 +36,7 @@ describe(
 );
 
 describe(
-  "fullCite()",
+  "fullConsume()",
   () => {
     [
       {
@@ -50,9 +51,42 @@ describe(
       it(
         test.results.join(","),
         () => {
-          chai.expect(fullCite(test.results).toString()).to.equal(test.expected);
+          chai.expect(fullConsume(test.results).join(",")).to.equal(test.expected);
         },
       );
+    });
+  },
+);
+
+describe(
+  "rootReducer()",
+  () => {
+    [
+      {
+        results: [],
+        expected: [],
+      },
+      {
+        results: [new MapperResult("379", "379", MapperType.NUMBER), new MapperResult("U.S.", "U.S.", MapperType.REPORTER), new MapperResult("241", "241", MapperType.NUMBER)],
+        expected: "3:379 U.S. 241",
+      },
+      {
+        results: [new MapperResult("Id", "Id", MapperType.ID), new MapperResult("0", "0", MapperType.NUMBER), new MapperResult("379", "379", MapperType.NUMBER), new MapperResult("U.S.", "U.S.", MapperType.REPORTER), new MapperResult("241", "241", MapperType.NUMBER)],
+        expected: "1:Id,3:379 U.S. 241"],
+      },
+      {
+        results: [new MapperResult("379", "379", MapperType.NUMBER), new MapperResult("379", "379", MapperType.NUMBER), new MapperResult("U.S.", "U.S.", MapperType.REPORTER), new MapperResult("U.S.", "U.S.", MapperType.REPORTER), new MapperResult("241", "241", MapperType.NUMBER)],
+        expected: [],
+      },
+      {
+        results: [new MapperResult("379", "379", MapperType.NUMBER), new MapperResult("U.S.", "U.S.", MapperType.REPORTER), new MapperResult("Id", "Id", MapperType.ID)],
+        expected: ["1:Id"],
+      },
+    ].forEach((test) => {
+      it(test.results.join(","), () => {
+        // @ts-ignore
+        chai.expect(coalesce(test.results).join(",")).to.equal(test.expected);
+      });
     });
   },
 );
