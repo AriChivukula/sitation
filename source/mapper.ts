9a89df4ce0fc9d1cc77ccd7057d152b94a89503b
Ari Chivukula <ari@chivuku.la>
2019-01-22 17:24:15 -0800

    Update mapper.ts

--- source/mapper.ts
+++ source/mapper.ts
@@ -10,32 +10,16 @@ export enum MapperType {
   REPORTER, // Can be found in `REPORTER_SET`
 }
 
-export class MapperPart {
+export class MapperResult {
 
   constructor(
     readonly corrected: string,
     readonly original: string,
-    readonly token: MapperType,
+    readonly type: MapperType,
   ) {
   }
 
   public toString(): string {
-    return this.corrected + ":" + this.original + ":" + this.token;
-  }
-}
-
-export class MapperParts {
-
-  constructor(
-    readonly parts: MapperPart[],
-  ) {
-  }
-  
-  public slice(begin: number, end?: number): MapperParts {
-    return new MapperParts(this.parts.slice(begin, end));
-  }
-
-  public toString(): string {
-    return this.parts.map((part) => part.toString()).join(",");
+    return this.corrected + ":" + this.original + ":" + this.type;
   }
 }
