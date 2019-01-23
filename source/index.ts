86fe14178c9316f8f2f7b5646590a79c88bab722
Ari Chivukula <ari@chivuku.la>
2019-01-23 10:18:38 -0800

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,33 +1,12 @@
 import "@babel/polyfill";
 
 import {
-  ReducerResult,
-} from "./reducer";
-import {
   rootReducer,
 } from "./reducers";
-import {
-  MapperResult,
-} from "./mapper";
 import {
   rootMapper,
 } from "./mappers";
 
-export function coalesce(results: MapperResult[]): string[] {
-  const citations: string[] = [];
-  let remaining = results;
-  while (remaining.length > 0) {
-    const result = rootReducer(remaining);
-    if (result.isNOOP()) {
-      remaining = remaining.slice(1);
-      continue;
-    }
-    citations.push(result.content);
-    remaining = remaining.slice(result.consumed);
-  }
-  return citations;
-}
-
 export function sitation(casebody: string): string[] {
-  return coalesce(rootMapper(casebody));
+  return rootReducer(rootMapper(casebody)).map((result) => result.content);
 }
