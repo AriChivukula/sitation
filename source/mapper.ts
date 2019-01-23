6aeaaa43058be07c09a6a4439d86110735b7ef5b
Ari Chivukula <ari@chivuku.la>
2019-01-22 16:29:10 -0800

    Update mapper.ts

--- source/mapper.ts
+++ source/mapper.ts
@@ -39,28 +39,3 @@ export class MapperParts {
     return this.parts.map((part) => part.toString()).join(",");
   }
 }
-
-export function tokenize(casebody: string): Segmented {
-  const tokens: Segment[] = [];
-  for (let reporter_split_token of casebody.split(Expressions.reporter())) {
-    const lower_split_token = reporter_split_token.toLowerCase();
-    if (ReportersDB.editions().hasOwnProperty(lower_split_token)) {
-      tokens.push(new MapperResult(ReportersDB.editions()[lower_split_token], reporter_split_token, MapperType.REPORTER));
-    } else if (ReportersDB.variations().hasOwnProperty(lower_split_token)) {
-      tokens.push(new MapperResult(ReportersDB.variations()[lower_split_token], reporter_split_token, MapperType.REPORTER));
-    } else {
-      for (let spacing_split_token of reporter_split_token.split(Expressions.spacing())) {
-        if (spacing_split_token == "") {
-          continue;
-        } else if (!isNaN(Number(spacing_split_token))) {
-          tokens.push(new MapperResult(spacing_split_token, spacing_split_token, MapperType.NUMBER))
-        } else if (spacing_split_token.toLowerCase() == "id") {
-          tokens.push(new MapperResult("Id", spacing_split_token, MapperType.ID));
-        } else {
-          tokens.push(new MapperResult(spacing_split_token, spacing_split_token, MapperType.NOOP))
-        }
-      }
-    }
-  }
-  return new MapperParts(tokens);
-}
