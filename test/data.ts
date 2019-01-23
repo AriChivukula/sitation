4e9a94443d4b13fefc8a0ec2a1469c8cc878b655
Ari Chivukula <ari@chivuku.la>
2019-01-22 16:01:08 -0800

    Map/Reduce

--- test/data.ts
+++ test/data.ts
@@ -6,30 +6,31 @@ import {
   ReportersDB,
 } from "../source/data";
 
-it(
-  "reporters",
+describe(
+  "ReportersDB",
   async (): Promise<void> => {
-    chai.expect(Object.keys(ReportersDB.reporters()).length).to.equal(457);
-    chai.expect(Object.keys(ReportersDB.reporters())[0]).to.equal("A.");
-  },
-);
-
-
-it(
-  "editions",
-  async (): Promise<void> => {
-    chai.expect(Object.keys(ReportersDB.editions()).length).to.equal(529);
-    chai.expect(Object.keys(ReportersDB.editions())[0]).to.equal("a.");
-    chai.expect(Object.values(ReportersDB.editions())[0]).to.equal("A.");
-  },
-);
-
-
-it(
-  "variations",
-  async (): Promise<void> => {
-    chai.expect(Object.keys(ReportersDB.variations()).length).to.equal(1014);
-    chai.expect(Object.keys(ReportersDB.variations())[0]).to.equal("a. 2d");
-    chai.expect(Object.values(ReportersDB.variations())[0]).to.equal("A.2d");
+    it(
+      "reporters",
+      async (): Promise<void> => {
+        chai.expect(Object.keys(ReportersDB.reporters()).length).to.equal(457);
+        chai.expect(Object.keys(ReportersDB.reporters())[0]).to.equal("A.");
+      },
+    );
+    it(
+      "editions",
+      async (): Promise<void> => {
+        chai.expect(Object.keys(ReportersDB.editions()).length).to.equal(529);
+        chai.expect(Object.keys(ReportersDB.editions())[0]).to.equal("a.");
+        chai.expect(Object.values(ReportersDB.editions())[0]).to.equal("A.");
+      },
+    );
+    it(
+      "variations",
+      async (): Promise<void> => {
+        chai.expect(Object.keys(ReportersDB.variations()).length).to.equal(1014);
+        chai.expect(Object.keys(ReportersDB.variations())[0]).to.equal("a. 2d");
+        chai.expect(Object.values(ReportersDB.variations())[0]).to.equal("A.2d");
+      },
+    );
   },
 );
