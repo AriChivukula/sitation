2b89d884ac40e4f323e4a1db9b770d7ccd87e762
Ari Chivukula <ari@chivuku.la>
2019-01-22 17:31:54 -0800

    Update mappers.ts

--- source/mappers.ts
+++ source/mappers.ts
@@ -5,32 +5,31 @@ import {
   ReportersDB,
 } from "./data";
 import {
-  MapperPart,
-  MapperParts,
+  MapperResult,
   MapperType,
 } from "./mapper";
 
-export function rootMapper(casebody: string): MapperParts {
-  const parts: MapperPart[] = [];
+export function rootMapper(casebody: string): MapperResult[] {
+  const results: MapperResult[] = [];
   for (let reporter_split_token of casebody.split(Expressions.reporter())) {
     const lower_split_token = reporter_split_token.toLowerCase();
     if (ReportersDB.editions().hasOwnProperty(lower_split_token)) {
-      parts.push(new MapperPart(ReportersDB.editions()[lower_split_token], reporter_split_token, MapperType.REPORTER));
+      results.push(new MapperResult(ReportersDB.editions()[lower_split_token], reporter_split_token, MapperType.REPORTER));
     } else if (ReportersDB.variations().hasOwnProperty(lower_split_token)) {
-      parts.push(new MapperPart(ReportersDB.variations()[lower_split_token], reporter_split_token, MapperType.REPORTER));
+      results.push(new MapperResult(ReportersDB.variations()[lower_split_token], reporter_split_token, MapperType.REPORTER));
     } else {
       for (let spacing_split_token of reporter_split_token.split(Expressions.spacing())) {
         if (spacing_split_token == "") {
           continue;
         } else if (!isNaN(Number(spacing_split_token))) {
-          parts.push(new MapperPart(spacing_split_token, spacing_split_token, MapperType.NUMBER))
+          results.push(new MapperResult(spacing_split_token, spacing_split_token, MapperType.NUMBER))
         } else if (spacing_split_token.toLowerCase() == "id") {
-          parts.push(new MapperPart("Id", spacing_split_token, MapperType.ID));
+          results.push(new MapperResult("Id", spacing_split_token, MapperType.ID));
         } else {
-          parts.push(new MapperPart(spacing_split_token, spacing_split_token, MapperType.NOOP))
+          results.push(new MapperResult(spacing_split_token, spacing_split_token, MapperType.NOOP))
         }
       }
     }
   }
-  return new MapperParts(parts);
+  return results;
 }
