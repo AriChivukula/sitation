4f144f45b9a0f6febc562eac32099186d4bfbaf5
Ari Chivukula <ari@chivuku.la>
2019-01-23 10:35:14 -0800

    Update reducers.ts

--- source/reducers.ts
+++ source/reducers.ts
@@ -9,33 +9,33 @@ import {
   MapperType,
 } from "./mapper";
 
-export function idConsume(resuts: MapperResult[]): ReducerResult {
+export function idConsume(resuts: MapperResult[]): ReducerResult[] {
   if (resuts.length < 1) {
-    return ReducerResult.noop();
+    return [];
   }
   if (resuts[0].type !== MapperType.ID) {
-    return ReducerResult.noop();
+    return [];
   }
-  return new ReducerResult(1, resuts[0].corrected);
+  return [new ReducerResult(1, resuts[0].corrected)];
 }
 
-export function fullConsume(resuts: MapperResult[]): ReducerResult {
+export function fullConsume(resuts: MapperResult[]): ReducerResult[] {
   if (resuts.length < 3) {
-    return ReducerResult.noop();
+    return [];
   }
   if (resuts[0].type !== MapperType.NUMBER) {
-    return ReducerResult.noop();
+    return [];
   }
   if (resuts[1].type !== MapperType.REPORTER) {
-    return ReducerResult.noop();
+    return [];
   }
   if (resuts[2].type !== MapperType.NUMBER) {
-    return ReducerResult.noop();
+    return [];
   }
-  return new ReducerResult(
+  return [new ReducerResult(
     3,
     resuts.slice(0, 3).map((segment) => segment.corrected).join(" "),
-  );
+  )];
 }
 
 export const rootReducer = consumeAll(consumeEach([
