c43cfaa2256c3f5d800512bb992fb0c113394937
Ari Chivukula <ari@chivuku.la>
2019-01-22 16:28:41 -0800

    Update and rename token.ts to mapper.ts

--- source/mapper.ts
+++ source/mapper.ts
@@ -1,21 +1,21 @@
 import {
-  ReportersDB,
   Expressions,
+  ReportersDB,
 } from "./data";
 
-export enum Token {
+export enum MapperType {
   ID, // `Id` style citation
   NOOP, // Token used only to break up patterns
   NUMBER, // [0-9]+
   REPORTER, // Can be found in `REPORTER_SET`
 }
 
-export class Segment {
+export class MapperPart {
 
   constructor(
     readonly corrected: string,
     readonly original: string,
-    readonly token: Token,
+    readonly token: MapperType,
   ) {
   }
 
@@ -24,19 +24,19 @@ export class Segment {
   }
 }
 
-export class Segmented {
+export class MapperParts {
 
   constructor(
-    readonly segments: Segment[],
+    readonly parts: MapperPart[],
   ) {
   }
   
-  public slice(begin: number, end?: number): Segmented {
-    return new Segmented(this.segments.slice(begin, end));
+  public slice(begin: number, end?: number): MapperParts {
+    return new MapperParts(this.parts.slice(begin, end));
   }
 
   public toString(): string {
-    return this.segments.map((segment) => segment.toString()).join(",");
+    return this.parts.map((part) => part.toString()).join(",");
   }
 }
 
@@ -45,22 +45,22 @@ export function tokenize(casebody: string): Segmented {
   for (let reporter_split_token of casebody.split(Expressions.reporter())) {
     const lower_split_token = reporter_split_token.toLowerCase();
     if (ReportersDB.editions().hasOwnProperty(lower_split_token)) {
-      tokens.push(new Segment(ReportersDB.editions()[lower_split_token], reporter_split_token, Token.REPORTER));
+      tokens.push(new MapperResult(ReportersDB.editions()[lower_split_token], reporter_split_token, MapperType.REPORTER));
     } else if (ReportersDB.variations().hasOwnProperty(lower_split_token)) {
-      tokens.push(new Segment(ReportersDB.variations()[lower_split_token], reporter_split_token, Token.REPORTER));
+      tokens.push(new MapperResult(ReportersDB.variations()[lower_split_token], reporter_split_token, MapperType.REPORTER));
     } else {
       for (let spacing_split_token of reporter_split_token.split(Expressions.spacing())) {
         if (spacing_split_token == "") {
           continue;
         } else if (!isNaN(Number(spacing_split_token))) {
-          tokens.push(new Segment(spacing_split_token, spacing_split_token, Token.NUMBER))
+          tokens.push(new MapperResult(spacing_split_token, spacing_split_token, MapperType.NUMBER))
         } else if (spacing_split_token.toLowerCase() == "id") {
-          tokens.push(new Segment("Id", spacing_split_token, Token.ID));
+          tokens.push(new MapperResult("Id", spacing_split_token, MapperType.ID));
         } else {
-          tokens.push(new Segment(spacing_split_token, spacing_split_token, Token.NOOP))
+          tokens.push(new MapperResult(spacing_split_token, spacing_split_token, MapperType.NOOP))
         }
       }
     }
   }
-  return new Segmented(tokens);
+  return new MapperParts(tokens);
 }
