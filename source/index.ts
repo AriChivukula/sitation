1250833b849f980b489c5f27b51f46ab1a76da5d
Ari Chivukula <ari@chivuku.la>
2019-01-22 16:18:30 -0800

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,12 +1,28 @@
 import "@babel/polyfill";
 
 import {
-  coalesce,
-} from "./match";
+  rootReducer,
+} from "./reducers";
 import {
+  Segmented,
   tokenize,
 } from "./token";
 
 export function sitation(casebody: string): string[] {
   return coalesce(tokenize(casebody));
+}
+
+function coalesce(segmented: Segmented): string[] {
+  const citations: string[] = [];
+  let remaining = segmented;
+  while (remaining.segments.length > 0) {
+    const result = rootReducer(remaining);
+    if (result.isNOOP()) {
+      remaining = remaining.slice(1);
+      continue;
+    }
+    citations.push(result.content);
+    remaining = remaining.slice(result.consumed);
+  }
+  return citations;
 }
