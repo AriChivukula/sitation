d335758badf3f648b0bc6f7a38e65cfe74459219
Ari Chivukula <ari@chivuku.la>
2019-01-22 17:34:04 -0800

    Update reducers.ts

--- source/reducers.ts
+++ source/reducers.ts
@@ -4,36 +4,36 @@ import {
   serialReducers,
 } from "./reducer";
 import {
-  MapperParts,
+  MapperResult,
   MapperType,
 } from "./mapper";
 
-export function idCite(parts: MapperParts): ReducerResult {
-  if (parts.parts.length < 1) {
+export function idCite(resuts: MapperResult[]): ReducerResult {
+  if (resuts.length < 1) {
     return ReducerResult.noop();
   }
-  if (parts.parts[0].token !== MapperType.ID) {
+  if (resuts[0].type !== MapperType.ID) {
     return ReducerResult.noop();
   }
-  return new ReducerResult(1, parts.parts[0].corrected);
+  return new ReducerResult(1, resuts[0].corrected);
 }
 
-export function fullCite(parts: MapperParts): ReducerResult {
-  if (parts.parts.length < 3) {
+export function fullCite(resuts: MapperResult[]): ReducerResult {
+  if (resuts.length < 3) {
     return ReducerResult.noop();
   }
-  if (parts.parts[0].token !== MapperType.NUMBER) {
+  if (resuts[0].type !== MapperType.NUMBER) {
     return ReducerResult.noop();
   }
-  if (parts.parts[1].token !== MapperType.REPORTER) {
+  if (resuts[1].type !== MapperType.REPORTER) {
     return ReducerResult.noop();
   }
-  if (parts.parts[2].token !== MapperType.NUMBER) {
+  if (resuts[2].type !== MapperType.NUMBER) {
     return ReducerResult.noop();
   }
   return new ReducerResult(
     3,
-    parts.parts.slice(0, 3).map((segment) => segment.corrected).join(" "),
+    resuts.slice(0, 3).map((segment) => segment.corrected).join(" "),
   );
 }
 
