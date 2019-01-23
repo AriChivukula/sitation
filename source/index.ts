29792bc76c6c35c898ee2d82b349af32fab6e0f3
Ari Chivukula <ari@chivuku.la>
2019-01-22 16:30:07 -0800

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -4,12 +4,34 @@ import {
   rootReducer,
 } from "./reducers";
 import {
-  Segmented,
-  tokenize,
+  MapperResult,
+  MapperResults,
+  MapperType,
 } from "./token";
 
-export function sitation(casebody: string): string[] {
-  return coalesce(tokenize(casebody));
+export function tokenize(casebody: string): MapperParts {
+  const tokens: Segment[] = [];
+  for (let reporter_split_token of casebody.split(Expressions.reporter())) {
+    const lower_split_token = reporter_split_token.toLowerCase();
+    if (ReportersDB.editions().hasOwnProperty(lower_split_token)) {
+      tokens.push(new MapperResult(ReportersDB.editions()[lower_split_token], reporter_split_token, MapperType.REPORTER));
+    } else if (ReportersDB.variations().hasOwnProperty(lower_split_token)) {
+      tokens.push(new MapperResult(ReportersDB.variations()[lower_split_token], reporter_split_token, MapperType.REPORTER));
+    } else {
+      for (let spacing_split_token of reporter_split_token.split(Expressions.spacing())) {
+        if (spacing_split_token == "") {
+          continue;
+        } else if (!isNaN(Number(spacing_split_token))) {
+          tokens.push(new MapperResult(spacing_split_token, spacing_split_token, MapperType.NUMBER))
+        } else if (spacing_split_token.toLowerCase() == "id") {
+          tokens.push(new MapperResult("Id", spacing_split_token, MapperType.ID));
+        } else {
+          tokens.push(new MapperResult(spacing_split_token, spacing_split_token, MapperType.NOOP))
+        }
+      }
+    }
+  }
+  return new MapperParts(tokens);
 }
 
 export function coalesce(segmented: Segmented): string[] {
@@ -25,4 +47,8 @@ export function coalesce(segmented: Segmented): string[] {
     remaining = remaining.slice(result.consumed);
   }
   return citations;
+}
+
+export function sitation(casebody: string): string[] {
+  return coalesce(tokenize(casebody));
 }
