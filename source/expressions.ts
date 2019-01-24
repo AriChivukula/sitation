2a8c4789606085df5be64cef584c10a6b3f9473f
Ari Chivukula <ari@chivuku.la>
2019-01-24 07:52:51 -0800

    Update and rename data.ts to expressions.ts

---
+++ source/expressions.ts
@@ -0,0 +1,36 @@
+import {
+  MemoizeAll,
+} from "lodash-decorators";
+
+import {
+  DB,
+} from "./db";
+
+export abstract class Expressions {
+
+  @MemoizeAll
+  public static escape(): RegExp {
+    return /[-[\]{}()*+!<=:?.\/\\^$|#\s,]/g;
+  }
+
+  @MemoizeAll()
+  public static reporter(): RegExp {
+    const editionsAndVariations = Object.keys(Object.assign({}, DB.editions(), DB.signals()));
+    editionsAndVariations.sort((a, b) => b.length - a.length);
+    const escape = (s: string) => s.replace(Expressions.escape(), "\\$&");
+    return RegExp("\\s(" + editionsAndVariations.map(escape).join("|") + ")\\s", "i");
+  }
+
+  @MemoizeAll()
+  public static signal(): RegExp {
+    const signals = Object.keys(DB.signals());
+    signals.sort((a, b) => b.length - a.length);
+    const escape = (s: string) => s.replace(Expressions.escape(), "\\$&");
+    return RegExp("\\s(" + signals.map(escape).join("|") + ")\\s", "i");
+  }
+
+  @MemoizeAll
+  public static spacing(): RegExp {
+    return /[\s,;:.()[\]{}]+/;
+  }
+}
