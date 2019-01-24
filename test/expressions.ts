de6901de31c2b6bb5ffbf6e3e73adfa147a02aa0
Ari Chivukula <ari@chivuku.la>
2019-01-24 13:24:57 -0800

    Update expressions.ts

--- test/expressions.ts
+++ test/expressions.ts
@@ -18,7 +18,7 @@ describe(
     it(
       "range",
       async (): Promise<void> => {
-        chai.expect(Expressions.range().toString().length).to.equal(13);
+        chai.expect(Expressions.range().toString().length).to.equal(12);
       },
     );
     it(
