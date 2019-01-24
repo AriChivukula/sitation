63fa1ebba90152e9e53c51f3a8b76c6889d9c03a
Ari Chivukula <ari@chivuku.la>
2019-01-23 17:51:20 -0800

    Update mapper.ts

--- source/mapper.ts
+++ source/mapper.ts
@@ -7,7 +7,7 @@ export enum MapperType {
   ID, // `Id` style citation
   NOOP, // Token used only to break up patterns
   NUMBER, // [0-9]+
-  RANGE, // e.g. 419-20
+  PINPOINT, // e.g. 419-20
   REPORTER, // e.g. U.S.
   SIGNAL, // e.g. see also
 }
@@ -37,7 +37,7 @@ export class MapperResult {
     return new MapperResult(original, original, MapperType.NUMBER);
   }
   
-  public static range(original: string) {
+  public static pinpoint(original: string) {
     return new MapperResult(original, original, MapperType.RANGE);
   }
 
