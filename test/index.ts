cdb900946f9a750d9e23983117163f0c25a8fe28
Ari Chivukula <ari@chivuku.la>
2019-01-23 16:54:36 -0800

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -12,25 +12,25 @@ describe(
     [
       {
         casebody: "",
-        expected: [],
+        expected: "",
       },
       {
         casebody: "379 u. S. 241",
-        expected: ["379 U.S. 241"],
+        expected: "379 U.S. 241",
       },
       {
         casebody: "In Evans v. Laurel Links, Inc., 261 F. Supp. 474, 477 (ED Va. 1966), a class action brought",
-        expected: ["261 F. Supp. 474"],
+        expected: "261 F. Supp. 474",
       },
       {
         casebody: "See, e. g., Birchem v. Knights of Columbus, 116 F.3d 310, 312-313 (CA8 1997); cf. Nationwide Mut. Ins. Co. v. Darden, 503 U. S. 318, 322323 (1992); Id. at 435",
-        expected: ["116 F.3d 310", "503 U.S. 318", "Id"],
+        expected: "116 F.3d 310\n503 U.S. 318\nId",
       },
     ].forEach((test) => {
       it(
         test.casebody,
         () => {
-          chai.expect(sitation(test.casebody)).to.deep.equal(test.expected);
+          chai.expect(sitation(test.casebody)).to.equal(test.expected);
         },
       );
     });
