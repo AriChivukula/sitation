9005621e6a95efa1589633e5fa7958b1f0beb876
Ari Chivukula <ari@chivuku.la>
2019-01-24 08:02:21 -0800

    Update db.ts

--- source/db.ts
+++ source/db.ts
@@ -30,8 +30,8 @@ export abstract class DB {
   @MemoizeAll()
   public static reporters(): Variations {
     let result: Variations = {};
-    for (let reporterName in ReportersDB.reporters()) {
-      for (let reporter of ReportersDB.reporters()[reporterName]) {
+    for (let reporterName in DB.reporters()) {
+      for (let reporter of DB.reporters()[reporterName]) {
         for (let edition in reporter.editions) {
           result[edition.toLowerCase()] = edition;
         }
