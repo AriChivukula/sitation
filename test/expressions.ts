c16d48196022f2c00ae56fb49c8288ce2d5c7d2d
Ari Chivukula <ari@chivuku.la>
2019-01-26 14:03:51 -0500

    Update expressions.ts

--- test/expressions.ts
+++ test/expressions.ts
@@ -12,35 +12,35 @@ describe(
     it(
       "escape()",
       async (): Promise<void> => {
-        chai.expect("{a".match(Expressions.escape())).to.deep.equal(["{"]);
+        chai.expect("{a".split(Expressions.escape())).to.deep.equal(["{"]);
       },
     );
     it(
       "range()",
       async (): Promise<void> => {
-        chai.expect("419-21".match(Expressions.range())).to.deep.equal(["419-21"]);
-        chai.expect("419!21".match(Expressions.range())).to.be.null;
+        chai.expect("419-21".split(Expressions.range())).to.deep.equal(["419-21"]);
+        chai.expect("419!21".split(Expressions.range())).to.deep.equal(["419", "!", "21"]);
       },
     );
     it(
       "reporter()",
       async (): Promise<void> => {
-        chai.expect(" U.S. ".match(Expressions.reporter())).to.deep.equal(["U.S."]);
-        chai.expect(" U.P.S. ".match(Expressions.reporter())).to.be.null;
+        chai.expect(" U.S. ".split(Expressions.reporter())).to.deep.equal(["U.S."]);
+        chai.expect(" U.P.S. ".split(Expressions.reporter())).to.deep.equal([" U.P.S. "]);
       },
     );
     it(
       "signal()",
       async (): Promise<void> => {
-        chai.expect("See".match(Expressions.signal())).to.deep.equal(["See"]);
-        chai.expect("Not".match(Expressions.signal())).to.be.null;
+        chai.expect("See also FOO".split(Expressions.signal())).to.deep.equal(["See also", "FOO"]);
+        chai.expect("Not also BAR".split(Expressions.signal())).to.deep.equal(["Not also BAR"]);
       },
     );
     it(
       "spacing()",
       async (): Promise<void> => {
-        chai.expect(" ".match(Expressions.spacing())).to.deep.equal([" "]);
-        chai.expect("a".match(Expressions.spacing())).to.be.null;
+        chai.expect("a b".split(Expressions.spacing())).to.deep.equal(["a", " ", "b"]);
+        chai.expect("ab".split(Expressions.spacing())).to.deep.equal(["ab"]);
       },
     );
   },
