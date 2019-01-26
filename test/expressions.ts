581f4dd06f9bea91a8aff729391f26763217983a
Ari Chivukula <ari@chivuku.la>
2019-01-26 13:51:13 -0500

    Update expressions.ts

--- test/expressions.ts
+++ test/expressions.ts
@@ -12,35 +12,35 @@ describe(
     it(
       "escape()",
       async (): Promise<void> => {
-        chai.expect("{a".match(Expressions.escape())).to.equal(["{"]);
+        chai.expect("{a".match(Expressions.escape())).to.deep.equal(["{"]);
       },
     );
     it(
       "range()",
       async (): Promise<void> => {
-        chai.expect("419-21".match(Expressions.range())).to.equal(["419-21"]);
-        chai.expect("419!21".match(Expressions.range())).to.equal([]);
+        chai.expect("419-21".match(Expressions.range())).to.deep.equal(["419-21"]);
+        chai.expect("419!21".match(Expressions.range())).to.deep.equal([]);
       },
     );
     it(
       "reporter()",
       async (): Promise<void> => {
-        chai.expect("U.S.".match(Expressions.reporter())).to.equal(["U.S."]);
-        chai.expect("U.P.S.".match(Expressions.reporter())).to.equal([]);
+        chai.expect("U.S.".match(Expressions.reporter())).to.deep.equal(["U.S."]);
+        chai.expect("U.P.S.".match(Expressions.reporter())).to.deep.equal([]);
       },
     );
     it(
       "signal()",
       async (): Promise<void> => {
-        chai.expect("See also".match(Expressions.signal())).to.equal(["See also"]);
-        chai.expect("Not also".match(Expressions.signal())).to.equal([]);
+        chai.expect("See also".match(Expressions.signal())).to.deep.equal(["See also"]);
+        chai.expect("Not also".match(Expressions.signal())).to.deep.equal([]);
       },
     );
     it(
       "spacing()",
       async (): Promise<void> => {
-        chai.expect(" ".match(Expressions.spacing())).to.equal([" "]);
-        chai.expect("a".match(Expressions.spacing())).to.equal([]);
+        chai.expect(" ".match(Expressions.spacing())).to.deep.equal([" "]);
+        chai.expect("a".match(Expressions.spacing())).to.deep.equal([]);
       },
     );
   },
