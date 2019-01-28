b265f9a2b13661008d3052db9c9516055717b82c
Ari Chivukula <ari@chivuku.la>
2019-01-28 10:50:04 -0800

    Update mapper.ts

--- test/mapper.ts
+++ test/mapper.ts
@@ -12,13 +12,23 @@ describe(
   () => {
     [
       {
+        description: "Empty",
         mappers: [],
-        token: "EMPTY TEST",
+        token: "blah",
         expected: [],
+      },
+      {
+        description: "Basic",
+        mappers: [
+          () => [],
+          () => [MapperResult.number("1776")]
+        ],
+        token: "blah",
+        expected: [MapperResult.number("1776")],
       },
     ].forEach((test) => {
       it(
-        test.token,
+        test.description,
         () => {
           chai.expect(matchFirst(test.mappers)(test.token).join("\n")).to.equal(test.expected.join("\n"));
         },
@@ -32,14 +42,29 @@ describe(
   () => {
     [
       {
+        description: "Empty",
         splitterFN: () => [],
         mapperFN: () => [],
-        token: "EMPTY TEST",
+        token: "blah",
         expected: [],
+      },
+      {
+        description: "Basic",
+        splitterFN: (token: string) => token.split("la"),
+        mapperFN: (token: string) => {
+          if (token === "b") {
+            return [MapperResult.number("1776")];
+          } else if (token === "h") {
+            return [MapperResult.number("1812")];
+          }
+          return [MapperResult.number("2016")];
+        },
+        token: "blah",
+        expected: [MapperResult.number("1776"), MapperResult.number("1812")],
       },
     ].forEach((test) => {
       it(
-        test.token,
+        test.description,
         () => {
           chai.expect(matchSplit(test.splitterFN, test.mapperFN)(test.token).join("\n")).to.equal(test.expected.join("\n"));
         },
