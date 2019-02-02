402df1b6f7be7ec0a9eb4eaadab3bad325c6584f
Ari Chivukula <ari@chivuku.la>
2019-02-02 11:59:20 -0800

    Update auto.sh

--- test/auto.sh
+++ test/auto.sh
@@ -1,16 +1,15 @@
 set -e
 
-export TEST_INS="$(ls test/auto | grep '.in')"
+export TEST_INS="${ls test/auto | grep '.in'}"
 for TEST_IN in $TEST_INS
 do
   echo "RUNNING ${TEST_IN}"
-  export CASEBODY="$(cat test/auto/${TEST_IN})"
+  export CASEBODY="${cat test/auto/${TEST_IN}}"
   export TEST_OUT="${TEST_IN::-3}.out"
-  export EXPECTED_OUT="$(cat test/auto/${TEST_OUT})"
-  node build/cli.js --casebody "${CASEBODY}"
-  export ACTUAL_OUT="$(node build/cli.js --casebody \"${CASEBODY}\")"
+  export EXPECTED_OUT="${cat test/auto/${TEST_OUT}}"
+  export ACTUAL_OUT="${node build/cli.js --casebody \"${CASEBODY}\"}"
   echo "ACTUAL_OUT ${ACTUAL_OUT}"
-  export DIFF="$(diff <(echo ${EXPECTED_OUT}) <(echo ${ACTUAL_OUT}))"
+  export DIFF="${diff <(echo ${EXPECTED_OUT}) <(echo ${ACTUAL_OUT})}"
   if [ -n "$DIFF" ]; then
     echo "FAILURE"
     echo $DIFF
