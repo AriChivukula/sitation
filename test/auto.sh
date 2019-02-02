18223034e464d11f9c5571be258f6d17adddbb6e
Ari Chivukula <ari@chivuku.la>
2019-02-02 11:39:38 -0800

    Update auto.sh

--- test/auto.sh
+++ test/auto.sh
@@ -4,14 +4,15 @@ export TEST_INS="$(ls test/auto | grep '.in')"
 for TEST_IN in $TEST_INS
 do
   echo "TEST_IN ${TEST_IN}"
-  export CASEBODY="$(cat TEST_IN)"
+  export CASEBODY="$(cat test/auto/TEST_IN)"
   echo "CASEBODY ${CASEBODY}"
   export TEST_OUT="${TEST_IN::-3}.out"
   echo "TEST_OUT ${TEST_OUT}"
-  export EXPECTED_OUT="$(cat TEST_OUT)"
+  export EXPECTED_OUT="$(cat test/auto/TEST_OUT)"
   echo "EXPECTED_OUT ${EXPECTED_OUT}"
   echo "RUNNING ${TEST_IN}"
   export ACTUAL_OUT="$(node build/cli.js --casebody ${CASEBODY})"
   echo "ACTUAL_OUT ${ACTUAL_OUT}"
-  diff  <(echo "$EXPECTED_OUT" ) <(echo "$ACTUAL_OUT")
+  export DIFF="$(diff <(echo $EXPECTED_OUT) <(echo $ACTUAL_OUT))"
+  echo "DIFF ${DIFF}"
 done
