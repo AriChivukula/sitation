31670e3a5e771599eb024ec340692c41693a8dd4
Ari Chivukula <ari@chivuku.la>
2019-02-02 11:52:17 -0800

    Update auto.sh

--- test/auto.sh
+++ test/auto.sh
@@ -7,15 +7,14 @@ do
   export CASEBODY="$(cat test/auto/${TEST_IN})"
   export TEST_OUT="${TEST_IN::-3}.out"
   export EXPECTED_OUT="$(cat test/auto/${TEST_OUT})"
-  export ACTUAL_OUT="$(node build/cli.js --casebody ${CASEBODY})"
+  export ACTUAL_OUT="$(node build/cli.js --casebody \"${CASEBODY}\")"
   echo "ACTUAL_OUT ${ACTUAL_OUT}"
   export DIFF="$(diff <(echo ${EXPECTED_OUT}) <(echo ${ACTUAL_OUT}))"
-  echo "DIFF ${DIFF}"
   if [ -n "$DIFF" ]; then
-    echo "PASS"
-  else
     echo "FAILURE"
     echo "${DIFF}"
     exit 1
+  else
+    echo "PASS"
   fi
 done
