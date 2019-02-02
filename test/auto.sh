0b4c2b5a090a51979ababd467d88679ec3c0ee84
Ari Chivukula <ari@chivuku.la>
2019-02-02 12:10:01 -0800

    Update auto.sh

--- test/auto.sh
+++ test/auto.sh
@@ -8,7 +8,6 @@ do
   export TEST_OUT="${TEST_IN::-3}.out"
   export EXPECTED_OUT="$(cat test/auto/${TEST_OUT})"
   export ACTUAL_OUT="$(node build/cli.js --casebody "${CASEBODY}")"
-  echo "ACTUAL_OUT ${ACTUAL_OUT}"
   export DIFF="$(diff <(echo ${EXPECTED_OUT}) <(echo ${ACTUAL_OUT}))"
   if [ -n "$DIFF" ]; then
     echo "FAILURE"
