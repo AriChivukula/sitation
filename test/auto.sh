27dd8c7ed46b678ffb17d3cd65e4a37ade75be61
Ari Chivukula <ari@chivuku.la>
2019-02-02 11:27:42 -0800

    Update auto.sh

--- test/auto.sh
+++ test/auto.sh
@@ -1,6 +1,11 @@
 set -e
 
+export TESTS="$(ls test/auto)"
+echo "RUNNING ${TESTS}"
+export TESTS="$(ls test/auto | grep '.in')"
+echo "RUNNING ${TESTS}"
 export TESTS="$(ls test/auto | grep '.in' | awk '{print $4}')"
+echo "RUNNING ${TESTS}"
 for TEST in $TESTS
 do
   echo "RUNNING ${TEST}"
