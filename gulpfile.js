0701c2fa6f6166411b15c9c973b485b326f34049
Ari Chivukula <ari@chivuku.la>
2019-05-19 17:03:18 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,24 @@ var ts = require("gulp-typescript");
 var project = ts.createProject("tsconfig.json");
 
 gulp.task(
-  "build",
-  () => gulp.src("source/cli.ts", "source/index.ts")
+  "build:1",
+  () => gulp.src("source/db.ts")
     .pipe(replace("EMBED_REPORTERS_DB", fs.readFileSync("reporters-db/reporters_db/data/reporters.json", "ascii")))
+    .pipe(gulp.dest("source/")),
+);
+
+gulp.task(
+  "build:2",
+  () => gulp.src("source/cli.ts", "source/index.ts")
     .pipe(project())
     .js
     .pipe(gulp.dest("build/")),
+);
+
+gulp.task(
+  "build",
+  gulp.series(
+    "build:1",
+    "build:2",
+  ),
 );
